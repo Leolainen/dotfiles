751ab18e1442a9ddcc441a45dc51e502a7a937ac
Leolainen <leolainen2@gmail.com>
2025-02-24 08:45:17 +0100

    [sketchybar] add dynamic theme derived from parsed ghostty themes

--- config/sketchybar/themes.sh
+++ config/sketchybar/themes.sh
@@ -1,3 +1,5 @@
+source "$HOME/.config/sketchybar/palette.sh"
+source "$HOME/.config/sketchybar/typography.sh"
 
 # surface: 100
 # overlay: 200
@@ -82,7 +84,7 @@ theme_nordic() {
     export BORDER_WIDTH=1
     export BORDER_RADIUS=11
     export FONT="LiterationMono Nerd Font" # Needs to have Regular, Bold, Semibold, Heavy and Black variants
-    export PADDING=12 # All paddings use this value (icon, label, background)
+    export PADDING=12                      # All paddings use this value (icon, label, background)
     export LABEL_SM="$FONT:book:10.0"
     export LABEL_MD="$FONT:book:12.0"
     export LABEL_LG="$FONT:book:14.0"
@@ -126,33 +128,33 @@ theme_nordic() {
     export BORDER=0x80908caa
     export BORDER_HIGHLIGHT=$BLUE_200
 }
-    # # dark_black = '#2e3440', -- bg
-    # black = '#3b4252',
-    # bright_black = '#434c5e',
-    # gray = '#4c566a',
+# # dark_black = '#2e3440', -- bg
+# black = '#3b4252',
+# bright_black = '#434c5e',
+# gray = '#4c566a',
 
-    # -- Custom
-    # darkish_black = '#3B414D',
-    # grayish = '#667084',
-    # dark_black_alt = '#2B303B',
+# -- Custom
+# darkish_black = '#3B414D',
+# grayish = '#667084',
+# dark_black_alt = '#2B303B',
 
-    # -- Snow Storm
-    # dark_white = '#d8dee9',
-    # # white = '#e5e9f0',
-    # # bright_white = '#eceff4',
+# -- Snow Storm
+# dark_white = '#d8dee9',
+# # white = '#e5e9f0',
+# # bright_white = '#eceff4',
 
-    # -- Frost
-    # cyan = '#8fbcbb', -- classes, types and primitives.
-    # bright_cyan = '#88c0d0', -- declarations, calls and execution statements of functions, methods and routines.
-    # blue = '#81a1c1', -- keywords, support characters, operators, tags, units, punctuations
-    # intense_blue = '#5e81ac', -- pragmas, comment keywords and pre-processor statements.
+# -- Frost
+# cyan = '#8fbcbb', -- classes, types and primitives.
+# bright_cyan = '#88c0d0', -- declarations, calls and execution statements of functions, methods and routines.
+# blue = '#81a1c1', -- keywords, support characters, operators, tags, units, punctuations
+# intense_blue = '#5e81ac', -- pragmas, comment keywords and pre-processor statements.
 
-    # # -- Aurora
-    # # red = '#bf616a', -- deletions and errors
-    # # orange = '#d08770', -- annotations and decorators
-    # # yellow = '#ebcb8b', -- modifications, warning and escape characters
-    # # green = '#a3be8c', -- additions and string
-    # # purple = '#b48ead', -- integers and floating point numbers
+# # -- Aurora
+# # red = '#bf616a', -- deletions and errors
+# # orange = '#d08770', -- annotations and decorators
+# # yellow = '#ebcb8b', -- modifications, warning and escape characters
+# # green = '#a3be8c', -- additions and string
+# # purple = '#b48ead', -- integers and floating point numbers
 
 # Background Color: #24211E
 # Foreground Color: #D7C484
@@ -177,7 +179,7 @@ theme_darkearth() {
     export BORDER_RADIUS=0
 
     # Separate font for icons
-    export ICON_FONT="LiterationMono Nerd Font" 
+    export ICON_FONT="LiterationMono Nerd Font"
 
     # Departure Mono font
     export FONT="Departure Mono"
@@ -187,7 +189,7 @@ theme_darkearth() {
     export ICON_SM="$ICON_FONT:book:12.0"
     export ICON_MD="$ICON_FONT:book:14.0"
     export ICON_LG="$ICON_FONT:book:16.0"
-    
+
     # ProggyCleanSZ font
     # export FONT="ProggyCleanSZ Nerd Font Mono"
     # export LABEL_SM="$FONT:book:10.0"
@@ -196,9 +198,9 @@ theme_darkearth() {
     # export ICON_SM="$FONT:bold:12.0"
     # export ICON_MD="$FONT:bold:14.0"
     # export ICON_LG="$FONT:bold:16.0"
-    
+
     # BigBlueTermPlus Font
-    # export FONT="BigBlueTermPlus Nerd Font Mono" 
+    # export FONT="BigBlueTermPlus Nerd Font Mono"
     # export LABEL_SM="$FONT:regular:12.0"
     # export LABEL_MD="$FONT:regular:14.0"
     # export LABEL_LG="$FONT:regular:16.0"
@@ -221,9 +223,8 @@ theme_darkearth() {
     # export GREY_500=0xff8fbcbb
     export TRANSPARENT=0x00000000
 
-
     # semantic colors
-    export TEXT_PRIMARY=0xffD7C484 # 0xffB36B42 # 0xffD7C484 # 0xffB36B42
+    export TEXT_PRIMARY=0xffD7C484   # 0xffB36B42 # 0xffD7C484 # 0xffB36B42
     export TEXT_SECONDARY=0xffB36B42 # 0xff5F865F
     export TEXT_HIGHLIGHT=0xff5F865F
     export ERROR=$RED
@@ -233,13 +234,82 @@ theme_darkearth() {
     export ACCENT=$YELLOW
     export CANVAS=0xff24211E
     # export BACKGROUND=$GREEN
-    export BACKGROUND=0xff675642 #0xff24211E
+    export BACKGROUND=0xff675642           #0xff24211E
     export BACKGROUND_HIGHLIGHT=0xff24211E # 0xffD7C484
     export BORDER=0xff675642
     export BORDER_HIGHLIGHT=0xff675642
+}
+
+layout() {
+    # base tokens
+    export SPACING=8
+    export ITEM_HEIGHT=20
+    export BAR_HEIGHT=32
+    export PADDING=12 # All paddings use this value (icon, label, background)
+    export BLUR=0
+}
+
+borders() {
+    # Thin rounded borders
+    # export BORDER_WIDTH=1
+    # export BORDER_RADIUS=11
+
+    # Thick square borders
+    export BORDER_WIDTH=4
+    export BORDER_RADIUS=0
+
+}
+
+color_base() {
+    # https://www.ditig.com/256-colors-cheat-sheet
+    export BLACK="${palette[0]}"
+    export MAROON="${palette[1]}"
+    export GREEN="${palette[2]}"
+    export OLIVE="${palette[3]}"
+    export NAVY="${palette[4]}"
+    export PURPLE="${palette[5]}"
+    export TEAL="${palette[6]}"
+    export SILVER="${palette[7]}"
+    export GREY="${palette[8]}"
+    export RED="${palette[9]}"
+    export LIME="${palette[10]}"
+    export YELLOW="${palette[11]}"
+    export BLUE="${palette[12]}"
+    export FUCHSIA="${palette[13]}"
+    export AQUA="${palette[14]}"
+    export WHITE="${palette[15]}"
+    export TRANSPARENT=0x00000000
+}
+
+color_semantic() {
+    color_base
+
+    export TEXT_PRIMARY="$foreground"
+    # export TEXT_PRIMARY="$WHITE"
+    export TEXT_SECONDARY="$GREY"
+    export TEXT_HIGHLIGHT="$selection_background"
+    export ERROR="$RED"
+    export SUCCESS="$GREEN"
+    export INFO="$BLUE"
+    export WARNING="$OLIVE"
+    export ACCENT="$YELLOW"
+    export CANVAS="$background"
+    export BACKGROUND="$background"
+    export BACKGROUND_HIGHLIGHT="$selection_background"
+    export BORDER="${GREY}"
+    export BORDER_HIGHLIGHT="$SILVER"
+}
+
+theme_dynamic() {
+    typography_pixel
+    layout
+    borders
+    color_base
+    color_semantic
 }
 
 # Export the functions
 export -f theme_rose_pine
 export -f theme_nordic
 export -f theme_darkearth
+export -f theme_dynamic
