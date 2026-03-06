194fce4954f3af2e0fe8c05ee1746f4dad1ff098
Leolainen <leolainen2@gmail.com>
2026-03-06 09:37:19 +0100

    [sketchybar] removes duplicate sketchybar dir

--- config/sketchybar/colors.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-source "$HOME/.config/sketchybar/themes.sh" # Loads all defined colors
-
-# theme_rose_pine
-# theme_nordic
-# theme_darkearth
-theme_dynamic
--- config/sketchybar/plugins/calendar_update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-sketchybar  --set   calendar    icon="" label="$(date '+%a %d. %b')"
-sketchybar  --set   clock    icon="" label="$(date '+%H:%M')"
